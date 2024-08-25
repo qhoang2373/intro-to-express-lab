9d0ad66451e01f8b200905ba7d1f7f9d5371b438
Quan <qhoang2373@gmail.com>
2024-08-25 13:48:07 -0700

    added an app.get syntax

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.get('/greetings/Quan', (req, res) => {
+    const username = req.params.username;
+    res.send(`Hello there, ${username}!`)
+});
+
 // Listen for requests on port 3000
 app.listen(3000, () => {
     console.log('Listening on port 3000')
