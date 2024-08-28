c504dd7de2f2c26f1a6bc389eb20ef5746f09c3c
Quan <qhoang2373@gmail.com>
2024-08-27 19:19:20 -0700

    added Exercise 2 question

--- server.js
+++ server.js
@@ -22,20 +22,3 @@ app.listen(3000, () => {
 // Examples: Matches routes like /roll/6 or /roll/20.
 // Validation: If the parameter is not a number, respond with “You must specify a number.” For instance, /roll/potato should trigger this response.
 // Functionality: If a valid number is provided, respond with a random whole number between 0 and the given number. For example, a request to /roll/16 might respond with “You rolled a 14.”
-
-app.get('/roll/:number', (req, res) => {
-    const number = req.params.number;
-    res.send()
-
-});
-
-// Exercise 3: I Want THAT One!
-// Task: Create a route for URLs like /collectibles/<index-parameter>
-// Examples: Mathes routes such as /collectibles/2 or /collectibles/0
-// Data Array:
-
-const collectibles = [
-    { name: 'shiny ball', price: 5.95 },
-    { name: 'autographed picture of a dog', price: 10 },
-    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
-  ];
