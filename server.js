9dca4bb436de06417bf03d10d6e895d066b72fef
Quan <qhoang2373@gmail.com>
2024-08-27 19:56:57 -0700

    add Exercise 3 data array

--- server.js
+++ server.js
@@ -36,6 +36,12 @@ app.get('/roll/:number', (req, res) => {
 // Data Array:
 
 
+const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
 
 
 
